5163650b9cdc2c61061d5368b6c7be22f28b7b40
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2023-08-29 21:00:35 +0100

    Some modifications in BOARD section

--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -32,19 +32,157 @@ const Board = () => {
             {/* {% include 'navbar-all.html' %} */}
             <div className="projectContainer">
               <div className='projectName'>
-                <p>Back <b>Board Name</b></p>
+                <p><span className='pe-2'><i className="fa fa-arrow-left"></i></span>Board Name</p>
               </div>
               <div className='projectDescription'>
                 <p>This is a safe space for the project description</p>
               </div>
-              <div className='boardTasks'>
-                <div className='boardTaskName'>
-                  <p>Task Name</p>
-                  <div className='subTasks'>
-                    <p>For Subtask</p>
+              <div className='row gx-4 boardParent'>
+
+                <div className='col-md-3'>
+                  <div className='p-1 eachboard'>
+                    <div className='eachboardsubdiv'>
+                      <input className='boardNameInput' type='text' />
+                      <button className='deleteBoard'><span><i className='fa fa-xmark'></i></span></button>
+                    </div>
+                    <div className='boardTasks'>
+                      <div className='eachboardTask'>
+                        <p>Proof Of Concept</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Regression Test</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Setup monitoring and controlling process</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Requirement analysis completed</p>
+                      </div>
+                      <div className='eachboard_addTask p-1'>
+                        <button>+ Add Task</button>
+                      </div>
+                    </div>
+                    
                   </div>
                 </div>
+
+                <div className='col-md-3'>
+                  <div className='p-1 eachboard'>
+                    <div className='eachboardsubdiv'>
+                      <input className='boardNameInput' type='text' />
+                      <button className='deleteBoard'><span><i className='fa fa-xmark'></i></span></button>
+                    </div>
+                    <div className='boardTasks'>
+                      <div className='eachboardTask'>
+                        <p>Proof Of Concept</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Regression Test</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Setup monitoring and controlling process</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Requirement analysis completed</p>
+                      </div>
+                      <div className='eachboard_addTask p-1'>
+                        <button>+ Add Task</button>
+                      </div>
+                    </div>
+                    
+                  </div>
+                </div>
+
+                <div className='col-md-3'>
+                  <div className='p-1'>
+                    <div className='eachboard'>
+                      <div className='eachboardsubdiv'>
+                        
+                        <input className='boardNameInput' type='text' />
+                        <button className='deleteBoard'><span><i className='fa fa-xmark'></i></span></button>
+                      </div>
+                      <div className='boardTasks'>
+                        <div className='eachboardTask'>
+                          <p>Proof Of Concept</p>
+                        </div>
+                        <div className='eachboardTask'>
+                          <p>Regression Test</p>
+                        </div>
+                        <div className='eachboardTask'>
+                          <p>Setup monitoring and controlling process</p>
+                        </div>
+                        <div className='eachboardTask'>
+                          <p>Requirement analysis completed</p>
+                        </div>
+                        <div className='eachboard_addTask p-1'>
+                          <button>+ Add Task</button>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='col-md-3'>
+                  <div className='p-1'>
+                    <div className='eachboard'>
+                      <div className='eachboardsubdiv'>
+                        
+                        <input className='boardNameInput' type='text' />
+                        <button className='deleteBoard'><span><i className='fa fa-xmark'></i></span></button>
+                      </div>
+                      <div className='boardTasks'>
+                        <div className='eachboardTask'>
+                          <p>Proof Of Concept</p>
+                        </div>
+                        <div className='eachboardTask'>
+                          <p>Regression Test</p>
+                        </div>
+                        <div className='eachboardTask'>
+                          <p>Setup monitoring and controlling process</p>
+                        </div>
+                        <div className='eachboardTask'>
+                          <p>Requirement analysis completed</p>
+                        </div>
+                        <div className='eachboard_addTask p-1'>
+                          <button>+ Add Task</button>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='col-md-3 p-0'>
+                  <div className='p-1 eachboard'>
+                    <div className='eachboardsubdiv'>
+                      <input className='boardNameInput' type='text' />
+                      <button className='deleteBoard'><span><i className='fa fa-xmark'></i></span></button>
+                    </div>
+                    <div className='boardTasks'>
+                      <div className='eachboardTask'>
+                        <p>Proof Of Concept</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Regression Test</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Setup monitoring and controlling process</p>
+                      </div>
+                      <div className='eachboardTask'>
+                        <p>Requirement analysis completed</p>
+                      </div>
+                      <div className='eachboard_addTask p-1'>
+                        <button>+ Add Task</button>
+                      </div>
+                    </div>
+                    
+                  </div>
+                  
+                </div>
+
               </div>
+
             </div>
         </div>
 
